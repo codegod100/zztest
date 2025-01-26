060677fb4f2a07d450e98b9e5da066ff33853196
Your Name <you@example.com>
2025-01-26 03:41:00 -0800

    yum

--- flake.nix
+++ flake.nix
@@ -1,7 +1,21 @@
 {
-  inputs.zig.url = "github:mitchellh/zig-overlay";
-
-  outputs = { self, zig, ... }: {
-    ...
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
   };
+inputs.zig.url = "github:mitchellh/zig-overlay";
+  outputs =
+    { nixpkgs, flake-utils, zig,... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system}.extend zig.overlays.default;
+      in
+      {
+        devShells.default = pkgs.mkShell { packages = [ pkgs.bashInteractive pkgs.zigpkgs.master]; };
+      }
+    );
 }
